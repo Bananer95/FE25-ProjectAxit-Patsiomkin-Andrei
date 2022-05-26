4b85a2436ce400b01aaaf2d963b08c4050696878
Bananer95 <Bananer95@mail.ru>
2022-05-26 21:43:07 +0200

    fada

--- Homeworks/homeworks/homework_35.tsx
+++ Homeworks/homeworks/homework_35.tsx
@@ -38,9 +38,53 @@ const users: Array<IUser> = [
 // Task 1
 
 const stringFromName: string = users
-  .map((name: IUser) => {
-    return name;
+  .map((users: IUser): string => {
+    return users.name;
   })
   .join(',');
 
-console.log(stringFromName);
+// Task  2
+
+const sumCars: any = users
+  .map((users: IUser): number | undefined => {
+    return users.cars?.length;
+  })
+  .reduce((acc: number | undefined, leng: number | undefined): number => {
+    if (leng === undefined) {
+      leng = 0;
+    }
+    return ((acc as number) += leng as number);
+  }, 0);
+
+// Task 3
+
+const sortArray = (arr: Array<IUser>): IUser[] => {
+  const secondArrayUsers = arr.sort((a: IUser, b: IUser) => {
+    return Number(b['hasEducation']) - Number(a['hasEducation']);
+  });
+  return secondArrayUsers;
+};
+
+//  Task 4
+
+const sortArrayByAnimals = (arr: Array<IUser>): IUser[] => {
+  const firstArrayByAnimals = arr.sort((a: IUser, b: IUser) => {
+    return (
+      Number(a.hasOwnProperty('animals')) - Number(b.hasOwnProperty('animals'))
+    );
+  });
+  return firstArrayByAnimals;
+};
+
+// с животным внизу, так как, изначально юзер с котом  идет первым
+
+// Task 5
+
+const stringAuto = (arr: any) => {
+  const newArray = arr
+    .map((users: IUser) => {
+      return users.cars;
+    })
+    .join(',');
+  return newArray.replace(/,+/g, ',');
+};
